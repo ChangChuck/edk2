63f8ae4997a42d339d9ea906147c5326160fecfd
Olivier Martin <olivier.martin@arm.com>
2014-08-15 14:34:25 +0100

    ArmPkg/ArmCacheMaintenanceLib: Added InstructionCacheRangeOperation()
    Change-Id: Ib3712344bf38e790a690c4f0bfe8a84e4af4c809

--- ArmPkg/Library/ArmCacheMaintenanceLib/ArmCacheMaintenanceLib.c
+++ ArmPkg/Library/ArmCacheMaintenanceLib/ArmCacheMaintenanceLib.c
@@ -16,24 +16,61 @@
 #include <Library/ArmLib.h>
 #include <Library/PcdLib.h>
 
+STATIC
 VOID
-CacheRangeOperation (
+DataCacheRangeOperation (
   IN  VOID            *Start,
   IN  UINTN           Length,
+  IN  CACHE_OPERATION CacheOperation,
   IN  LINE_OPERATION  LineOperation
   )
 {
   UINTN ArmCacheLineLength         = ArmDataCacheLineLength();
   UINTN ArmCacheLineAlignmentMask  = ArmCacheLineLength - 1;
+  UINTN ArmCacheOperationThreshold = PcdGet32(PcdArmCacheOperationThreshold);
 
-  // Align address (rounding down)
-  UINTN AlignedAddress = (UINTN)Start - ((UINTN)Start & ArmCacheLineAlignmentMask);
-  UINTN EndAddress     = (UINTN)Start + Length;
+  if ((CacheOperation != NULL) && (Length >= ArmCacheOperationThreshold)) {
+    ArmDrainWriteBuffer ();
+    CacheOperation ();
+  } else {
+    // Align address (rounding down)
+    UINTN AlignedAddress = (UINTN)Start - ((UINTN)Start & ArmCacheLineAlignmentMask);
+    UINTN EndAddress     = (UINTN)Start + Length;
 
-  // Perform the line operation on an address in each cache line
-  while (AlignedAddress < EndAddress) {
-    LineOperation(AlignedAddress);
-    AlignedAddress += ArmCacheLineLength;
+    // Perform the line operation on an address in each cache line
+    while (AlignedAddress < EndAddress) {
+      LineOperation(AlignedAddress);
+      AlignedAddress += ArmCacheLineLength;
+    }
+  }
+}
+
+STATIC
+VOID
+InstructionCacheRangeOperation (
+  IN  VOID            *Start,
+  IN  UINTN           Length,
+  IN  CACHE_OPERATION CacheOperation,
+  IN  LINE_OPERATION  LineOperation
+  )
+{
+  UINTN ArmCacheLineLength         = ArmInstructionCacheLineLength ();
+  UINTN ArmCacheLineAlignmentMask  = ArmCacheLineLength - 1;
+  UINTN ArmCacheOperationThreshold = PcdGet32 (PcdArmCacheOperationThreshold);
+
+  if ((CacheOperation != NULL) && (Length >= ArmCacheOperationThreshold)) {
+    ArmDrainWriteBuffer ();
+    CacheOperation ();
+  } else {
+    // Align address (rounding down)
+    UINTN AlignedAddress = (UINTN)Start - ((UINTN)Start & ArmCacheLineAlignmentMask);
+    UINTN EndAddress     = (UINTN)Start + Length;
+
+    // Perform the line operation on an address in each cache line
+    while (AlignedAddress < EndAddress) {
+      LineOperation (AlignedAddress);
+      AlignedAddress += ArmCacheLineLength;
+    }
   }
 }
 
@@ -63,8 +100,9 @@ InvalidateInstructionCacheRange (
   IN      UINTN                     Length
   )
 {
-  CacheRangeOperation (Address, Length, ArmCleanDataCacheEntryByMVA);
-  ArmInvalidateInstructionCache ();
+  DataCacheRangeOperation (Address, Length, ArmCleanDataCacheEntryByMVA);
+  InstructionCacheRangeOperation (Address, Length, NULL, ArmInvalidateInstructionCacheEntryByMVA);
+  ArmInvalidateTlb ();
   return Address;
 }
 
@@ -84,7 +122,7 @@ WriteBackInvalidateDataCacheRange (
   IN      UINTN                     Length
   )
 {
-  CacheRangeOperation(Address, Length, ArmCleanInvalidateDataCacheEntryByMVA);
+  DataCacheRangeOperation (Address, Length, ArmCleanInvalidateDataCache, ArmCleanInvalidateDataCacheEntryByMVA);
   return Address;
 }
 
@@ -104,7 +142,7 @@ WriteBackDataCacheRange (
   IN      UINTN                     Length
   )
 {
-  CacheRangeOperation(Address, Length, ArmCleanDataCacheEntryByMVA);
+  DataCacheRangeOperation (Address, Length, ArmCleanDataCache, ArmCleanDataCacheEntryByMVA);
   return Address;
 }
 
@@ -115,6 +153,6 @@ InvalidateDataCacheRange (
   IN      UINTN                     Length
   )
 {
-  CacheRangeOperation(Address, Length, ArmInvalidateDataCacheEntryByMVA);
+  DataCacheRangeOperation (Address, Length, NULL, ArmInvalidateDataCacheEntryByMVA);
   return Address;
 }
